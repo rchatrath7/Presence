f79836fc92d6f86430b8d11bd52c2a96a08a3741
Rakesh  Chatrath <rchatrath7@gmail.com>
2017-01-21 10:43:56 -0500

    Fix syntax error

--- presenceapi/pipeline.py
+++ presenceapi/pipeline.py
@@ -9,7 +9,7 @@ def save_profile(backend, user, response, *args, **kwargs):
 
     FacebookProfile.objects.create(
         user=user,
-        access_token=token
-        likes=graph.get_connections(id='me', connection_name='likes')
+        access_token=token,
+        likes=graph.get_connections(id='me', connection_name='likes'),
         tagged_places=graph.get_connections(id='me', connection_name='tagged_places')
     )
