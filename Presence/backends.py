2d658b82648ae1eaf1eb82d764c16cade59a3a0c
Rakesh  Chatrath <rchatrath7@gmail.com>
2017-01-21 04:31:37 -0500

    Add custom backend for Facebook OAuth

---
+++ Presence/backends.py
@@ -0,0 +1,11 @@
+from social_core.backends.facebook import FacebookOAuth2
+
+class CustomFacebookOAuth2(FacebookOAuth2):
+    def get_scope(self):
+        scope = super(CustomFacebookOAuth2, self).get_scope()
+        scope += [
+            'email', 'user_friends', 'user_birthday', 'user_hometown',
+            'user_likes', 'user_location', 'user_photos', 'user_relationships',
+            'user_tagged_places', 'user_work_history'
+        ]
+        return scope
