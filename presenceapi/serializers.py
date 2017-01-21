7f022af1f3defb0948d4dac9c013f27299cbb2e6
Rakesh  Chatrath <rchatrath7@gmail.com>
2017-01-21 10:44:12 -0500

    Add JSONField serialization

--- presenceapi/serializers.py
+++ presenceapi/serializers.py
@@ -4,9 +4,20 @@ from rest_framework import serializers
 # Models
 from presenceapi.models import FacebookProfile
 
+class JSONSerializerField(serializers.Field):
+    """ Serializer for JSONField -- required to make field writable"""
+
+    def to_internal_value(self, data):
+        return data
+    def to_representation(self, value):
+        return value
+
+
 class FacebookProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source='user.first_name')
     last_name = serializers.CharField(source='user.first_name')
+    likes = JSONSerializerField()
+    tagged_places = JSONSerializerField()
 
     class Meta:
         model = FacebookProfile
@@ -14,5 +25,7 @@ class FacebookProfileSerializer(serializers.ModelSerializer):
             "id",
             "first_name",
             "last_name",
-            "access_token"
+            "access_token",
+            "likes",
+            "tagged_places"
         )
