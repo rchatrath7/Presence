27a21264bb40359c287fd8e90fb54c9643dc121d
Rakesh  Chatrath <rchatrath7@gmail.com>
2017-01-20 22:42:25 -0500

    Add rudimentary Facebook Profile Model

--- presenceapi/models.py
+++ presenceapi/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# Rudimentary Profile Model
+class FacebookProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
+    profile_picture = models.CharField(max_length=100)
+    access_token = models.CharField(max_length=100)
