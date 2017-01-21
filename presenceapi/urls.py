a275af62cdda4a5d57b446de41cdd8986c97ad3f
Rakesh  Chatrath <rchatrath7@gmail.com>
2017-01-20 23:16:26 -0500

    Add API endpoints

--- presenceapi/urls.py
+++ presenceapi/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    
+    url(r'^api/users/$', views.FacebookProfileList.as_view()),
+    url(r'^api/users/(?P<pk>[0-9]+)/$', views.FacebookProfileDetails.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
